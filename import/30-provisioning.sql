9b65ba343774e9bee9ae2156e5878bcb1c0f297c
Evelyn Gurschler <evelyn.gurschler@bmw.de>
2022-05-25 13:54:17 +0200

    Add initcontainer

--- import/30-provisioning.sql
+++ import/30-provisioning.sql
@@ -17,20 +17,20 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: provisioning; Type: SCHEMA; Schema: -; Owner: portal
+-- Name: provisioning; Type: SCHEMA; Schema: -; Owner: provisioning
 --
 
 CREATE SCHEMA provisioning;
 
 
-ALTER SCHEMA provisioning OWNER TO portal;
+ALTER SCHEMA provisioning OWNER TO provisioning;
 
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- Name: client_sequence; Type: TABLE; Schema: provisioning; Owner: portal
+-- Name: client_sequence; Type: TABLE; Schema: provisioning; Owner: provisioning
 --
 
 CREATE TABLE provisioning.client_sequence (
@@ -38,10 +38,10 @@ CREATE TABLE provisioning.client_sequence (
 );
 
 
-ALTER TABLE provisioning.client_sequence OWNER TO portal;
+ALTER TABLE provisioning.client_sequence OWNER TO provisioning;
 
 --
--- Name: client_sequence_sequence_id_seq; Type: SEQUENCE; Schema: provisioning; Owner: portal
+-- Name: client_sequence_sequence_id_seq; Type: SEQUENCE; Schema: provisioning; Owner: provisioning
 --
 
 CREATE SEQUENCE provisioning.client_sequence_sequence_id_seq
@@ -53,17 +53,17 @@ CREATE SEQUENCE provisioning.client_sequence_sequence_id_seq
     CACHE 1;
 
 
-ALTER TABLE provisioning.client_sequence_sequence_id_seq OWNER TO portal;
+ALTER TABLE provisioning.client_sequence_sequence_id_seq OWNER TO provisioning;
 
 --
--- Name: client_sequence_sequence_id_seq; Type: SEQUENCE OWNED BY; Schema: provisioning; Owner: portal
+-- Name: client_sequence_sequence_id_seq; Type: SEQUENCE OWNED BY; Schema: provisioning; Owner: provisioning
 --
 
 ALTER SEQUENCE provisioning.client_sequence_sequence_id_seq OWNED BY provisioning.client_sequence.sequence_id;
 
 
 --
--- Name: identity_provider_sequence; Type: TABLE; Schema: provisioning; Owner: portal
+-- Name: identity_provider_sequence; Type: TABLE; Schema: provisioning; Owner: provisioning
 --
 
 CREATE TABLE provisioning.identity_provider_sequence (
@@ -71,10 +71,10 @@ CREATE TABLE provisioning.identity_provider_sequence (
 );
 
 
-ALTER TABLE provisioning.identity_provider_sequence OWNER TO portal;
+ALTER TABLE provisioning.identity_provider_sequence OWNER TO provisioning;
 
 --
--- Name: identity_provider_sequence_sequence_id_seq; Type: SEQUENCE; Schema: provisioning; Owner: portal
+-- Name: identity_provider_sequence_sequence_id_seq; Type: SEQUENCE; Schema: provisioning; Owner: provisioning
 --
 
 CREATE SEQUENCE provisioning.identity_provider_sequence_sequence_id_seq
@@ -86,31 +86,31 @@ CREATE SEQUENCE provisioning.identity_provider_sequence_sequence_id_seq
     CACHE 1;
 
 
-ALTER TABLE provisioning.identity_provider_sequence_sequence_id_seq OWNER TO portal;
+ALTER TABLE provisioning.identity_provider_sequence_sequence_id_seq OWNER TO provisioning;
 
 --
--- Name: identity_provider_sequence_sequence_id_seq; Type: SEQUENCE OWNED BY; Schema: provisioning; Owner: portal
+-- Name: identity_provider_sequence_sequence_id_seq; Type: SEQUENCE OWNED BY; Schema: provisioning; Owner: provisioning
 --
 
 ALTER SEQUENCE provisioning.identity_provider_sequence_sequence_id_seq OWNED BY provisioning.identity_provider_sequence.sequence_id;
 
 
 --
--- Name: client_sequence sequence_id; Type: DEFAULT; Schema: provisioning; Owner: portal
+-- Name: client_sequence sequence_id; Type: DEFAULT; Schema: provisioning; Owner: provisioning
 --
 
 ALTER TABLE ONLY provisioning.client_sequence ALTER COLUMN sequence_id SET DEFAULT nextval('provisioning.client_sequence_sequence_id_seq'::regclass);
 
 
 --
--- Name: identity_provider_sequence sequence_id; Type: DEFAULT; Schema: provisioning; Owner: portal
+-- Name: identity_provider_sequence sequence_id; Type: DEFAULT; Schema: provisioning; Owner: provisioning
 --
 
 ALTER TABLE ONLY provisioning.identity_provider_sequence ALTER COLUMN sequence_id SET DEFAULT nextval('provisioning.identity_provider_sequence_sequence_id_seq'::regclass);
 
 
 --
--- Data for Name: client_sequence; Type: TABLE DATA; Schema: provisioning; Owner: portal
+-- Data for Name: client_sequence; Type: TABLE DATA; Schema: provisioning; Owner: provisioning
 --
 
 COPY provisioning.client_sequence (sequence_id) FROM stdin;
@@ -118,7 +118,7 @@ COPY provisioning.client_sequence (sequence_id) FROM stdin;
 
 
 --
--- Data for Name: identity_provider_sequence; Type: TABLE DATA; Schema: provisioning; Owner: portal
+-- Data for Name: identity_provider_sequence; Type: TABLE DATA; Schema: provisioning; Owner: provisioning
 --
 
 COPY provisioning.identity_provider_sequence (sequence_id) FROM stdin;
@@ -128,21 +128,21 @@ COPY provisioning.identity_provider_sequence (sequence_id) FROM stdin;
 
 
 --
--- Name: client_sequence_sequence_id_seq; Type: SEQUENCE SET; Schema: provisioning; Owner: portal
+-- Name: client_sequence_sequence_id_seq; Type: SEQUENCE SET; Schema: provisioning; Owner: provisioning
 --
 
 SELECT pg_catalog.setval('provisioning.client_sequence_sequence_id_seq', 1, false);
 
 
 --
--- Name: identity_provider_sequence_sequence_id_seq; Type: SEQUENCE SET; Schema: provisioning; Owner: portal
+-- Name: identity_provider_sequence_sequence_id_seq; Type: SEQUENCE SET; Schema: provisioning; Owner: provisioning
 --
 
 SELECT pg_catalog.setval('provisioning.identity_provider_sequence_sequence_id_seq', 2, true);
 
 
 --
--- Name: client_sequence client_sequence_pkey; Type: CONSTRAINT; Schema: provisioning; Owner: portal
+-- Name: client_sequence client_sequence_pkey; Type: CONSTRAINT; Schema: provisioning; Owner: provisioning
 --
 
 ALTER TABLE ONLY provisioning.client_sequence
@@ -150,14 +150,14 @@ ALTER TABLE ONLY provisioning.client_sequence
 
 
 --
--- Name: identity_provider_sequence identity_provider_sequence_pkey; Type: CONSTRAINT; Schema: provisioning; Owner: portal
+-- Name: identity_provider_sequence identity_provider_sequence_pkey; Type: CONSTRAINT; Schema: provisioning; Owner: provisioning
 --
 
 ALTER TABLE ONLY provisioning.identity_provider_sequence
     ADD CONSTRAINT identity_provider_sequence_pkey PRIMARY KEY (sequence_id);
 
 --
--- Name: user_password_reset ; Type: Table; Schema: provisioning; Owner: portal
+-- Name: user_password_reset ; Type: Table; Schema: provisioning; Owner: provisioning
 --
 
 CREATE TABLE provisioning.user_password_resets (
@@ -166,7 +166,7 @@ CREATE TABLE provisioning.user_password_resets (
     reset_count integer DEFAULT 0 NOT NULL
 );
 
-ALTER TABLE provisioning.user_password_resets OWNER TO portal;
+ALTER TABLE provisioning.user_password_resets OWNER TO provisioning;
 --
 -- PostgreSQL database dump complete
 --
